3b3a65a88cc1e48f7aa50acdc733683b337f56e0
Matt Childs <matt.childs@slalom.com>
2019-05-23 11:49:16 +0100

    added fixes to var.name

--- aws_cloudwatch_event_target.sns_event.tf
+++ aws_cloudwatch_event_target.sns_event.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_event_target" "sns_event" {
-  rule      = "${aws_cloudwatch_event_rule.console.name}"
+  rule      = "${aws_cloudwatch_event_rule.event_rule.name}"
   target_id = "SendToSNS"
   arn       = "${aws_sns_topic.slack_notifier.arn}"
 }
