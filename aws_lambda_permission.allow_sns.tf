25556e4ca5a3d9a47ac75c6297b3c60d062b2a79
Matt Childs <matt.childs@slalom.com>
2019-05-24 13:03:56 +0100

    changed principle on lambda permission

--- aws_lambda_permission.allow_sns.tf
+++ aws_lambda_permission.allow_sns.tf
@@ -2,6 +2,6 @@ resource "aws_lambda_permission" "allow_sns" {
   statement_id  = "AllowExecutionFromSNSTopicSubscription_${var.pipeline_name}"
   action        = "lambda:InvokeFunction"
   function_name = "${data.aws_lambda_function.slack_notifier.function_name}"
-  principal     = "sns.amazonaws.com"
+  principal     = "events.amazonaws.com"
   source_arn    = "${aws_sns_topic_subscription.subscription.arn}"
 }
